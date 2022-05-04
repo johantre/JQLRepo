5ccc8875becdd209cff139e435c87dce4e7f4e6b
Johan Tre (ext.) <Johan.Tre@partner.bmw.be>
2022-05-04 18:44:39 +0200

    learning from mistakes: EMPTY as well (non-Sofico)

--- Boards/MilesBoardFilterVersions.sql
+++ Boards/MilesBoardFilterVersions.sql
@@ -39,3 +39,9 @@ project in (MILES4ALL, "MyBMW@SF") AND
 --MPT only
 (issueFunction in issuesInEpics("project = MILES4ALL AND issuetype = Epic AND (Labels not in (Technical_NL, Technical_BE, Sofico) OR Labels is EMPTY)") AND (Labels not in (Sofico) OR Labels is EMPTY)) OR 
 (issuetype = Story AND (Labels not in (Technical_NL, Technical_BE, Sofico) OR Labels is EMPTY) AND "Epic Link" is EMPTY)
+
+--Sofico only
+"Project Specific Labels" in (Sofico) OR labels in (Sofico)
+
+--Non Sofico 
+Labels not in (Sofico) OR Labels is EMPTY 
