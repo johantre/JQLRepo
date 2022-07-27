377d15573dc6462d3becce0402b342c0075234a2
Johan Tre (ext.) <Johan.Tre@partner.bmw.be>
2022-07-27 19:18:28 +0200

    using user:pass authentication from prop file.

--- JiraReporting/UpdateJiraQuery.sh
+++ JiraReporting/UpdateJiraQuery.sh
@@ -6,7 +6,10 @@ jiraprops="$DIR/env/prod.jira.properties"
 
 HOSTNAME=$(prop 'jira.host' $jiraprops)
 EXTENSION=$(prop 'jira.extention' $jiraprops)
-USERBASE64=$(prop 'jira.user.base64' $jiraprops)
+JIRAUSER=$(prop 'jira.user' $jiraprops)
+JIRAPASS=$(prop 'jira.pass' $jiraprops)
+
+#USERBASE64=$(prop 'jira.user.base64' $jiraprops)
 
 QUERYID=$2
 PAYLOAD=$3
@@ -14,10 +17,10 @@ PAYLOAD=$3
 printf "queryId=$QUERYID and payload=$PAYLOAD"
 printf "\n"
 
-echo curl -u qqmibi4:g#cZrpo8onqvaij -X PUT --data "$PAYLOAD" -H "Content-Type: application/json" $HOSTNAME$QUERYID$EXTENSION
+echo curl -u $JIRAUSER:$JIRAPASS -X PUT --data "$PAYLOAD" -H "Content-Type: application/json" $HOSTNAME$QUERYID$EXTENSION
 printf "\n"
 
-curl -u qqmibi4:g#cZrpo8onqvaij -X PUT --data "$PAYLOAD" -H "Content-Type: application/json" $HOSTNAME$QUERYID$EXTENSION
+curl -u $JIRAUSER:$JIRAPASS -X PUT --data "$PAYLOAD" -H "Content-Type: application/json" $HOSTNAME$QUERYID$EXTENSION
 
 
 
