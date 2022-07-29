865e46a7fb8742699114c4b9fe0dabce73b65e74
Johan Tre (ext.) <Johan.Tre@partner.bmw.be>
2022-07-29 16:43:14 +0200

    oops... forgot the commented line

--- JiraReporting/jiraUpdateQuery.sh
+++ JiraReporting/jiraUpdateQuery.sh
@@ -2,6 +2,7 @@
 DIR=$(dirname $0)
 source "$DIR/lib/lib.sh"
 jiraprops="$DIR/env/prod.jira.properties"
+jirasecretprops="$DIR/env/prod.jira.secret.properties"
 
 HOSTNAME=$(prop 'jira.host' $jiraprops)
 EXTENSION=$(prop 'jira.extension' $jiraprops)
@@ -9,7 +10,6 @@ EXTENSION=$(prop 'jira.extension' $jiraprops)
 if test -z "$JIRAUSER"
 then
   echo "Falling back to $DIR/env/prod.jira.secret.properties fore credentials"
-  jirasecretprops="$DIR/env/prod.jira.secret.properties"
   JIRAUSER=$(prop 'jira.user' $jirasecretprops)
   JIRAPASS=$(prop 'jira.pass' $jirasecretprops)
 fi
@@ -17,5 +17,5 @@ fi
 QUERYID=$2
 PAYLOAD=$3
 
-#curl -u "$JIRAUSER:$JIRAPASS" -X PUT --data "$PAYLOAD" -H "Content-Type: application/json" $HOSTNAME$QUERYID$EXTENSION
+curl -u "$JIRAUSER:$JIRAPASS" -X PUT --data "$PAYLOAD" -H "Content-Type: application/json" $HOSTNAME$QUERYID$EXTENSION
 
