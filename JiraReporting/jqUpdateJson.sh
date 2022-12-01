476d1537d2bd3accd7e162497617c9d122ed3037
Johan Tre (ext.) <Johan.Tre@partner.bmw.be>
2022-12-01 21:35:25 +0100

    weird permission invest...

--- JiraReporting/jqUpdateJson.sh
+++ JiraReporting/jqUpdateJson.sh
@@ -13,8 +13,9 @@ printf "\n"
 argsForLoop=(-c -r '.FilterQueries[].Id' "$JSONQUERYFILE")
 for i in $($JQCOMMAND "${argsForLoop[@]}") 
   do
-      forId=$(echo $i | tr -d '\r')   #remove weird chars from $i...      
+      forId=$(echo $i | tr -d '\r')   #remove weird chars from $i...
       argsExecCmd=(-c -r --arg idToSearch "$forId" '.FilterQueries[] | if .Id == $idToSearch then .Id, .Data else empty end' "$JSONQUERYFILE")
+      echo $idToSearch
       $JQCOMMAND "${argsExecCmd[@]}" \
           | sed 's/"/\"/g' \
           | xargs -d '\n' \
