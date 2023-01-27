74f8094745cca55fd13bb876c8c44e752fca7560
Johan Tre (ext.) <Johan.Tre@partner.bmw.be>
2023-01-27 19:23:02 +0100

    fixed Quick filters

--- Boards/MilesBoardFilterVersions.sql
+++ Boards/MilesBoardFilterVersions.sql
@@ -7,12 +7,12 @@ assignee in (qtd2005, qtd0593, qt76380, qtc6712, qtb4864, qxz084o, qxz0c91, qxz3
 (issueFunction in issuesInEpics("project = MILES4ALL AND issuetype = Epic AND (Labels in (Technical_NL, Technical_BE) AND Labels not in (Sofico))") AND (Labels not in (Sofico) OR (Labels is EMPTY))) OR 
 (issuetype = Story AND (Labels in (Technical_NL, Technical_BE)) and "Epic Link" is EMPTY)
 
---MPT only
+--MPT only Defect board
 (issueFunction in issuesInEpics("project = MILES4ALL AND issuetype = Epic AND (Labels not in (Technical_NL, Technical_BE, Sofico) OR Labels is EMPTY)") AND (Labels not in (Sofico) OR Labels is EMPTY)) OR 
 (issuetype = Story AND (Labels not in (Technical_NL, Technical_BE, Sofico) OR Labels is EMPTY) AND "Epic Link" is EMPTY)
 
 --Sofico only
-"Project Specific Labels" in (Sofico) OR labels in (Sofico)
+"Feature Team" in (Sofico)
 
---Non Sofico 
-Labels not in (Sofico) OR Labels is EMPTY 
+--MPT Sprint board
+"Feature Team" not in (Sofico) OR "Feature Team" is EMPTY
